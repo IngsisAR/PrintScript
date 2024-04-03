1f3872b3402695e0cad85d3dc04c7db65d85c5c6
PedroElizalde01 <pedro.elizalde@ing.austral.edu.ar>
2024-04-03 19:35:19 -0300

    tests for interpreter

--- interpreter/src/main/kotlin/AssigmentExpressionInterpreter.kt
+++ interpreter/src/main/kotlin/AssigmentExpressionInterpreter.kt
@@ -4,15 +4,15 @@ class AssigmentExpressionInterpreter(
     override fun interpret(node: ASTNode): Map<String, VariableInfo> {
         node as AssigmentExpression
         val id = node.left.name
-        val variable = variableMap[id]
-        if (variable == null || variable.isMutable == false) throw IllegalArgumentException("Variable not mutable or not found")
+        val variable = variableMap[id] ?: throw IllegalArgumentException("Variable not found")
+        if(variable.isMutable == false) throw IllegalArgumentException("Variable not mutable")
         val newValue =
             when (val right = node.right) {
                 is NumberLiteral -> right.value.toString()
                 is StringLiteral -> right.value
                 is BinaryExpression -> BinaryExpressionInterpreter(variableMap).interpret(right)
                 is Identifier -> IdentifierInterpreter(variableMap).interpret(right)
-                else -> throw IllegalArgumentException("Function not found")
+                else -> throw IllegalArgumentException("Node not found")
             }
         val updatedMap = variableMap.toMutableMap()
         updatedMap[id] = variable.copy(value = newValue.toString())
--- interpreter/src/test/kotlin/InterpreterTest.kt
+++ interpreter/src/test/kotlin/InterpreterTest.kt
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.Assertions.assertThrows
 
 class InterpreterTest {
@@ -58,6 +57,36 @@ class InterpreterTest {
     }
 
     @Test
+    fun interpretIdentifierVariableDeclaration() {
+        var interpreter = InterpreterImpl(
+            mapOf(
+                "a" to VariableInfo("number", "2",true),
+            )
+        )
+        val astNode =
+            VariableDeclaration(
+                declarations =
+                listOf(
+                    VariableDeclarator(
+                        id = Identifier(name = "b", start = 4, end = 5),
+                        type = TypeReference(type = "number", start = 7, end = 13),
+                        init = Identifier(name = "a", start = 4, end = 5),
+                        start = 4,
+                        end = 21,
+                    ),
+                ),
+                kind = "let",
+                start = 0,
+                end = 22,
+            )
+
+        interpreter = interpreter.interpret(astNode)
+        val resultVariableMap = interpreter.variableMap
+
+        assertVariableInfo(resultVariableMap, "b","number", true, "2")
+    }
+
+    @Test
     fun interpretMultipleVariableDeclaration() {
         var interpreter = InterpreterImpl()
         val astNode =
@@ -99,30 +128,58 @@ class InterpreterTest {
     }
 
     @Test
+    fun interpretErrorVariableDeclaration() {
+        val interpreter = InterpreterImpl()
+        val astNode =
+            VariableDeclaration(
+                declarations =
+                listOf(
+                    VariableDeclarator(
+                        id = Identifier(name = "a", start = 4, end = 5),
+                        type = TypeReference(type = "string", start = 7, end = 13),
+                        init = CallExpression(
+                            callee = Identifier(name="println", start=0, end=7),
+                            arguments = listOf(Identifier(name="c", start=8, end=9)),
+                            start=0,
+                            end=10),
+                        start = 4,
+                        end = 23,
+                    ),
+                ),
+                kind = "let",
+                start = 0,
+                end = 24,
+            )
+        assertThrows(IllegalArgumentException::class.java) {
+            interpreter.interpret(astNode)
+        }
+    }
+
+    @Test
     fun interpretNumberBinaryExpression() {
         var interpreter = InterpreterImpl()
         val astNode =
-                    VariableDeclaration(
-                        declarations =
-                            listOf(
-                                VariableDeclarator(
-                                    id = Identifier(name = "a", start = 4, end = 5),
-                                    type = TypeReference(type = "number", start = 7, end = 13),
-                                    init =
-                                        BinaryExpression(
-                                            left = NumberLiteral(value = 5.toBigDecimal(), start = 16, end = 17),
-                                            right = NumberLiteral(value = 6.toBigDecimal(), start = 20, end = 21),
-                                            operator = "+",
-                                            start = 16,
-                                            end = 21,
-                                        ),
-                                    start = 4,
+            VariableDeclaration(
+                declarations =
+                    listOf(
+                        VariableDeclarator(
+                            id = Identifier(name = "a", start = 4, end = 5),
+                            type = TypeReference(type = "number", start = 7, end = 13),
+                            init =
+                                BinaryExpression(
+                                    left = NumberLiteral(value = 5.toBigDecimal(), start = 16, end = 17),
+                                    right = NumberLiteral(value = 6.toBigDecimal(), start = 20, end = 21),
+                                    operator = "+",
+                                    start = 16,
                                     end = 21,
                                 ),
-                            ),
-                        kind = "let",
-                        start = 0,
-                        end = 22,
+                            start = 4,
+                            end = 21,
+                        ),
+                    ),
+                kind = "let",
+                start = 0,
+                end = 22,
             )
 
         interpreter = interpreter.interpret(astNode)
@@ -135,27 +192,27 @@ class InterpreterTest {
     fun interpretStringBinaryExpression() {
         var interpreter = InterpreterImpl()
         val astNode =
-                    VariableDeclaration(
-                        declarations =
-                            listOf(
-                                VariableDeclarator(
-                                    id = Identifier(name = "b", start = 4, end = 5),
-                                    type = TypeReference(type = "string", start = 7, end = 13),
-                                    init =
-                                        BinaryExpression(
-                                            left = StringLiteral(value = "hello", start = 16, end = 23),
-                                            right = StringLiteral(value = " world", start = 26, end = 34),
-                                            operator = "+",
-                                            start = 16,
-                                            end = 34,
-                                        ),
-                                    start = 4,
+            VariableDeclaration(
+                declarations =
+                    listOf(
+                        VariableDeclarator(
+                            id = Identifier(name = "b", start = 4, end = 5),
+                            type = TypeReference(type = "string", start = 7, end = 13),
+                            init =
+                                BinaryExpression(
+                                    left = StringLiteral(value = "hello", start = 16, end = 23),
+                                    right = StringLiteral(value = " world", start = 26, end = 34),
+                                    operator = "+",
+                                    start = 16,
                                     end = 34,
                                 ),
-                            ),
-                        kind = "let",
-                        start = 0,
-                        end = 35,
+                            start = 4,
+                            end = 34,
+                        ),
+                    ),
+                kind = "let",
+                start = 0,
+                end = 35,
             )
 
         interpreter = interpreter.interpret(astNode)
@@ -269,7 +326,22 @@ class InterpreterTest {
     }
 
     @Test
-    fun testNumberAssignmentExpression() {
+    fun interpretInvalidOperatorBinaryExpression() {
+        val interpreter = InterpreterImpl()
+        val astNode = BinaryExpression(
+            left = NumberLiteral(value = 5.toBigDecimal(), start = 16, end = 17),
+            right = NumberLiteral(value = 6.toBigDecimal(), start = 20, end = 21),
+            operator = ".",
+            start = 16,
+            end = 21,
+        )
+        assertThrows(IllegalArgumentException::class.java) {
+            interpreter.interpret(astNode)
+        }
+    }
+
+    @Test
+    fun interpretNumberAssignmentExpression() {
         var interpreter = InterpreterImpl(mapOf("a" to VariableInfo("number", null,true)))
         val astNodeA = ExpressionStatement(
             expression = AssigmentExpression(
@@ -286,7 +358,7 @@ class InterpreterTest {
     }
 
     @Test
-    fun testStringAssignmentExpression() {
+    fun interpretStringAssignmentExpression() {
         var interpreter = InterpreterImpl(mapOf("a" to VariableInfo("string", "hello",true)))
         val astNodeD = ExpressionStatement(
             expression = AssigmentExpression(
@@ -303,7 +375,55 @@ class InterpreterTest {
     }
 
     @Test
-    fun testNonexistentAssignmentExpression() {
+    fun interpretBinaryAssignmentExpression() {
+        var interpreter = InterpreterImpl(
+            mapOf(
+                "a" to VariableInfo("number", "4",true),
+            )
+        )
+        val astNodeBinaryExpression = ExpressionStatement(
+            expression = AssigmentExpression(
+                left = Identifier(name = "a", start = 0, end = 1),
+                right = BinaryExpression(
+                    left = NumberLiteral(value = 4.toBigDecimal(), start = 29, end = 30),
+                    right = NumberLiteral(value = 4.toBigDecimal(), start = 29, end = 30),
+                    operator = "/",
+                    start = 29,
+                    end = 34
+                ),
+                start = 0,
+                end = 5
+            ),
+            start = 0,
+            end = 6
+        )
+        interpreter = interpreter.interpret(astNodeBinaryExpression)
+        assertVariableInfo(interpreter.variableMap, "a", "number", true, "1")
+    }
+    @Test
+    fun interpretIdentifierAssignmentExpression() {
+        var interpreter = InterpreterImpl(
+            mapOf(
+                "a" to VariableInfo("string", "hello",true),
+                "d" to VariableInfo("string", "world",true)
+            )
+        )
+        val astNodeBinaryExpression = ExpressionStatement(
+            expression = AssigmentExpression(
+                left = Identifier(name = "a", start = 0, end = 1),
+                right = Identifier(name = "d", start = 18, end = 19),
+                start = 0,
+                end = 5
+            ),
+            start = 0,
+            end = 6
+        )
+        interpreter = interpreter.interpret(astNodeBinaryExpression)
+        assertVariableInfo(interpreter.variableMap, "a", "string", true, "world")
+    }
+
+    @Test
+    fun interpretNotFoundAssignmentExpression() {
         val interpreter = InterpreterImpl()
         val astNodeB = ExpressionStatement(
             expression = AssigmentExpression(
@@ -321,7 +441,7 @@ class InterpreterTest {
     }
 
     @Test
-    fun testConstantAssignmentExpression() {
+    fun interpretConstantAssignmentExpression() {
         val interpreter = InterpreterImpl(mapOf("a" to VariableInfo("string", "hello",false)))
         val astNode = ExpressionStatement(
             expression = AssigmentExpression(
@@ -339,37 +459,110 @@ class InterpreterTest {
     }
 
     @Test
-    fun testIdentifierAssignmentExpression() {
-        var interpreter = InterpreterImpl(
-            mapOf(
-                "a" to VariableInfo("string", "hello",true),
-                "d" to VariableInfo("string", "world",false)
-            )
-        )
-        val astNodeBinaryExpression = ExpressionStatement(
+    fun interpretErrorAssignmentExpression() {
+        val interpreter = InterpreterImpl(mapOf("a" to VariableInfo("string", "hello",true)))
+        val astNode = ExpressionStatement(
             expression = AssigmentExpression(
                 left = Identifier(name = "a", start = 0, end = 1),
-                right = Identifier(name = "d", start = 18, end = 19),
+                right = AssigmentExpression(
+                    left = Identifier(name = "a", start = 0, end = 1),
+                    right = StringLiteral(value = "world", start = 4, end = 5),
+                    start = 0,
+                    end = 5
+                ),
                 start = 0,
                 end = 5
             ),
             start = 0,
             end = 6
         )
-        interpreter = interpreter.interpret(astNodeBinaryExpression)
-        assertVariableInfo(interpreter.variableMap, "a", "string", true, "world")
+        assertThrows(IllegalArgumentException::class.java) {
+            interpreter.interpret(astNode)
+        }
+    }
+
+    @Test
+    fun interpretNotFoundIdentifierExpression() {
+        val interpreter = InterpreterImpl(
+            mapOf(
+                "a" to VariableInfo("string", "hello",true),
+                "b" to VariableInfo("number", "2",false),
+            )
+        )
+        val astNode = ExpressionStatement(
+            expression = CallExpression(
+                callee = Identifier(name="println", start=0, end=7),
+                arguments = listOf(Identifier(name="c", start=8, end=9)),
+                start=0,
+                end=10),
+            start=0,
+            end=11
+        )
+        assertThrows(IllegalArgumentException::class.java) {
+            interpreter.interpret(astNode)
+        }
+    }
+
+    @Test
+    fun interpretInvalidTypeIdentifierExpression() {
+        val interpreter = InterpreterImpl(
+            mapOf(
+                "a" to VariableInfo("string", "hello",true),
+                "b" to VariableInfo("number", "2",false),
+                "c" to VariableInfo("boolean", "true",false),
+            )
+        )
+        val astNode = ExpressionStatement(
+            expression = CallExpression(
+                callee = Identifier(name="println", start=0, end=7),
+                arguments = listOf(Identifier(name="c", start=8, end=9)),
+                start=0,
+                end=10),
+            start=0,
+            end=11
+        )
+        assertThrows(IllegalArgumentException::class.java) {
+            interpreter.interpret(astNode)
+        }
+    }
+
+    @Test
+    fun interpretIdentifierExpression() {
+        val interpreter = InterpreterImpl(mapOf("a" to VariableInfo("string", "hello",true)))
+        val astNode = NumberLiteral(value = 2.toBigDecimal(), start = 22, end = 23)
+        assertThrows(IllegalArgumentException::class.java) {
+            interpreter.interpret(astNode)
+        }
+    }
+
+    @Test
+    fun interpretInvalidCallExpression() {
+        val interpreter = InterpreterImpl(mapOf("a" to VariableInfo("string", "hello",true)))
+
+        val astNode = ExpressionStatement(
+            expression = CallExpression(
+                callee = Identifier(name="sum", start=0, end=7),
+                arguments = listOf(Identifier(name="a", start=8, end=9)),
+                start=0,
+                end=10),
+            start=0,
+            end=11
+        )
+        assertThrows(IllegalArgumentException::class.java) {
+            interpreter.interpret(astNode)
+        }
     }
 
     private fun assertVariableInfo(
         variableMap: Map<String, VariableInfo>,
-        variableName: String,
+        name: String,
         type: String,
         isMutable: Boolean,
         value: Any?
     ) {
-        assertTrue(variableMap.containsKey(variableName), "Variable $variableName should exist")
-        assertEquals(type, variableMap[variableName]?.type, "Type mismatch for $variableName")
-        assertEquals(isMutable, variableMap[variableName]?.isMutable, "Mutability mismatch for $variableName")
-        assertEquals(value, variableMap[variableName]?.value, "Value mismatch for $variableName")
+        assertTrue(variableMap.containsKey(name), "Variable $name should exist")
+        assertEquals(type, variableMap[name]?.type, "Type mismatch for $name")
+        assertEquals(isMutable, variableMap[name]?.isMutable, "Mutability mismatch for $name")
+        assertEquals(value, variableMap[name]?.value, "Value mismatch for $name")
     }
 }
