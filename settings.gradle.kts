898ed2a4b21381b72285d187e43709394776268f
Manuel Hernandez Carrasco <manu@MacBook-Pro.local>
2024-04-09 16:44:33 -0300

    solved publishing error

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "PrintScript"
+rootProject.name = "printscript"
 include("cli", "interpreter", "lexer", "parser", "utils", "formatter")
 include("sca")
