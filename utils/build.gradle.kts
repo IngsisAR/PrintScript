5be727fc910928c9ef98ac19517d537c869fe5fb
BraRIOS <braian.rios@ing.austral.edu.ar>
2024-04-10 18:37:39 -0300

    Removed unused dependency in utils build.gradle

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -10,10 +10,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies{
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
-}
-
 tasks.test {
     useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport)
