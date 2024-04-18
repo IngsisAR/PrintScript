2d97aa60702463ef3474dfd9510eba6eb0f77c5e
PedroElizaldeSirius <pedroelizalde@sirius.com.ar>
2024-04-18 03:20:28 -0300

    readInput()

--- interpreter/src/main/kotlin/CallExpressionInterpreter.kt
+++ interpreter/src/main/kotlin/CallExpressionInterpreter.kt
@@ -7,9 +7,17 @@ class CallExpressionInterpreter(
         require(node is CallExpression) { "Node must be an CallExpression" }
         return when (node.callee.name) {
             "println" -> ConsoleOutputter(variableMap).output(node)
-            "readEnv" -> return readEnvironmentVariables(node.arguments[0] as StringLiteral)
+            "readEnv" -> readEnvironmentVariables(node.arguments[0] as StringLiteral)
+            "readInput" -> readUserInput(node.arguments[0] as StringLiteral)
             else -> throw IllegalArgumentException("Function '${node.callee.name}' not found")
         }
+    }
+
+    private fun readUserInput(text: StringLiteral?): Any {
+        print(text?.value ?: "")
+        val input = readLine()
+        println(input)
+        return input!!
     }
 
     private fun readEnvironmentVariables(key: StringLiteral): String {
