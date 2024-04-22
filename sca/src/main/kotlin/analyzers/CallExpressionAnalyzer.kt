d6bd1ec9380864750e8fce72dcf05b891532aee7
AAugurusa <95092029+AAugurusa@users.noreply.github.com>
2024-04-21 22:07:08 -0300

    SCA ReadInput test finished

--- sca/src/main/kotlin/analyzers/CallExpressionAnalyzer.kt
+++ sca/src/main/kotlin/analyzers/CallExpressionAnalyzer.kt
@@ -3,8 +3,7 @@ package analyzers
 import ASTNode
 import CallExpression
 import Identifier
-import NumberLiteral
-import StringLiteral
+import Literal
 
 class CallExpressionAnalyzer : Analyzer {
     override fun analyze(
@@ -16,23 +15,43 @@ class CallExpressionAnalyzer : Analyzer {
         if (astNode !is CallExpression) return result
         val callee = astNode.callee
         result += IdentifierAnalyzer().analyze(callee, configMap, lineIndex)
-        val printlnNoExpressionArguments = configMap["printlnNoExpressionArguments"] ?: true
-        if (printlnNoExpressionArguments !is Boolean) {
-            if (printlnNoExpressionArguments is String) {
-                return "Invalid printlnNoExpressionArguments configuration value: \"$printlnNoExpressionArguments\", expected a boolean\n"
-            }
-            return "Invalid printlnNoExpressionArguments configuration value: $printlnNoExpressionArguments, expected a boolean\n"
+
+        val configPrintln = configMap["printlnNoExpressionArguments"] ?: true
+        var aux = checkIfAttributeIsInConfigMapAndIsCorrect(configPrintln, "printlnNoExpressionArguments")
+        if (aux.isNotEmpty()) {
+            return aux
         }
+
+        val configReadInput = configMap["printlnNoExpressionArguments"] ?: true
+        aux = checkIfAttributeIsInConfigMapAndIsCorrect(configReadInput, "readInputNoExpressionArguments")
+        if (aux.isNotEmpty()) {
+            return aux
+        }
+
         val arguments = astNode.arguments
-        if (callee.name == "println") {
-            if (arguments.isNotEmpty()) {
-                for (argument in arguments) {
-                    if (printlnNoExpressionArguments &&
-                        argument !is StringLiteral &&
-                        argument !is NumberLiteral &&
-                        argument !is Identifier
-                    ) {
-                        result += "No expressions in println function rule violated at (${lineIndex + 1}:${argument.start})\n"
+        when (callee.name) {
+            "println" -> {
+                if (arguments.isNotEmpty()) {
+                    for (argument in arguments) {
+                        if (configPrintln as Boolean &&
+                            argument !is Literal &&
+                            argument !is Identifier
+                        ) {
+                            result += "No expressions in println function rule violated at (${lineIndex + 1}:${argument.start})\n"
+                        }
+                    }
+                }
+            }
+
+            "readInput" -> {
+                if (arguments.isNotEmpty()) {
+                    for (argument in arguments) {
+                        if (configReadInput as Boolean &&
+                            argument !is Literal &&
+                            argument !is Identifier
+                        ) {
+                            result += "No expressions in readInput function rule violated at (${lineIndex + 1}:${argument.start})\n"
+                        }
                     }
                 }
             }
@@ -41,5 +60,18 @@ class CallExpressionAnalyzer : Analyzer {
             result += ExpressionAnalyzer().analyze(argument, configMap, lineIndex)
         }
         return result
+    }
+
+    private fun checkIfAttributeIsInConfigMapAndIsCorrect(
+        config: Any,
+        attribute: String,
+    ): String {
+        if (config !is Boolean) {
+            if (config is String) {
+                return "Invalid $attribute configuration value: \"$config\", expected a boolean\n"
+            }
+            return "Invalid $attribute configuration value: $config, expected a boolean\n"
+        }
+        return ""
     }
 }
